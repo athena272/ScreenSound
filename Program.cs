8fe6876198f80883863d44b858fd1e9f63a66a42
athena272 <guilhermera272@gmail.com>
2025-01-12 15:57:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+string welcomeMessage = "Boas vindas ao Screen Sound";
+Console.WriteLine(welcomeMessage);
